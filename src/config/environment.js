030cb176fd31249688b553cf2945732ce602a46e
Pablo Pavez <pabloandr@live.cl>
2023-07-17 00:13:39 -0400

    port

--- src/config/environment.js
+++ src/config/environment.js
@@ -4,7 +4,7 @@ import * as dotenv from "dotenv"
 dotenv.config()
 
 export default {
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.PORT || 4500,
     DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
     TOKEN_SECRET: process.env.TOKEN_SECRET
 }
